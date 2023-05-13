ad62b032d02df3e41cadc2cb5de4d44f3c299d6d
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2023-05-13 03:13:18 +0200

    Initial

---
+++ WindowsFormsControlLibrary1/UserControl1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace WindowsFormsControlLibrary1
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.colorButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.findIntersectionsButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.firstXBox = new System.Windows.Forms.TextBox();
+            this.firstYBox = new System.Windows.Forms.TextBox();
+            this.secondXBox = new System.Windows.Forms.TextBox();
+            this.secondYBox = new System.Windows.Forms.TextBox();
+            this.linesView = new System.Windows.Forms.ListView();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tableLayoutPanel1);
+            this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
+            this.splitContainer1.Size = new System.Drawing.Size(1159, 577);
+            this.splitContainer1.SplitterDistance = 732;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.colorButton, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.updateButton, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.removeButton, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.findIntersectionsButton, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.addButton, 4, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.firstXBox, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.firstYBox, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.secondXBox, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.secondYBox, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.linesView, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 76F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(419, 573);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // colorButton
+            // 
+            this.colorButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.colorButton.Location = new System.Drawing.Point(3, 93);
+            this.colorButton.Name = "colorButton";
+            this.colorButton.Size = new System.Drawing.Size(77, 39);
+            this.colorButton.TabIndex = 1;
+            this.colorButton.Text = "Color";
+            this.colorButton.UseVisualStyleBackColor = true;
+            // 
+            // updateButton
+            // 
+            this.updateButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.updateButton.Location = new System.Drawing.Point(86, 93);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(77, 39);
+            this.updateButton.TabIndex = 1;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            // 
+            // removeButton
+            // 
+            this.removeButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.removeButton.Location = new System.Drawing.Point(169, 93);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(77, 39);
+            this.removeButton.TabIndex = 2;
+            this.removeButton.Text = "Remove";
+            this.removeButton.UseVisualStyleBackColor = true;
+            // 
+            // findIntersectionsButton
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.findIntersectionsButton, 2);
+            this.findIntersectionsButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.findIntersectionsButton.Location = new System.Drawing.Point(252, 93);
+            this.findIntersectionsButton.Name = "findIntersectionsButton";
+            this.findIntersectionsButton.Size = new System.Drawing.Size(164, 39);
+            this.findIntersectionsButton.TabIndex = 3;
+            this.findIntersectionsButton.Text = "Find Intersections";
+            this.findIntersectionsButton.UseVisualStyleBackColor = true;
+            this.findIntersectionsButton.Click += new System.EventHandler(this.findIntersectionsButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.addButton.Location = new System.Drawing.Point(335, 3);
+            this.addButton.Name = "addButton";
+            this.tableLayoutPanel1.SetRowSpan(this.addButton, 2);
+            this.addButton.Size = new System.Drawing.Size(81, 84);
+            this.addButton.TabIndex = 5;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "First X";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(169, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Second X";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(169, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Second Y";
+            // 
+            // firstXBox
+            // 
+            this.firstXBox.Location = new System.Drawing.Point(86, 3);
+            this.firstXBox.Name = "firstXBox";
+            this.firstXBox.Size = new System.Drawing.Size(77, 24);
+            this.firstXBox.TabIndex = 10;
+            this.firstXBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // firstYBox
+            // 
+            this.firstYBox.Location = new System.Drawing.Point(86, 48);
+            this.firstYBox.Name = "firstYBox";
+            this.firstYBox.Size = new System.Drawing.Size(77, 24);
+            this.firstYBox.TabIndex = 10;
+            // 
+            // secondXBox
+            // 
+            this.secondXBox.Location = new System.Drawing.Point(252, 3);
+            this.secondXBox.Name = "secondXBox";
+            this.secondXBox.Size = new System.Drawing.Size(77, 24);
+            this.secondXBox.TabIndex = 10;
+            // 
+            // secondYBox
+            // 
+            this.secondYBox.Location = new System.Drawing.Point(252, 48);
+            this.secondYBox.Name = "secondYBox";
+            this.secondYBox.Size = new System.Drawing.Size(77, 24);
+            this.secondYBox.TabIndex = 10;
+            // 
+            // linesView
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.linesView, 5);
+            this.linesView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.linesView.HideSelection = false;
+            this.linesView.Location = new System.Drawing.Point(3, 138);
+            this.linesView.Name = "linesView";
+            this.linesView.Size = new System.Drawing.Size(413, 432);
+            this.linesView.TabIndex = 11;
+            this.linesView.UseCompatibleStateImageBehavior = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "First Y";
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(1159, 577);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button colorButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Button findIntersectionsButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox firstXBox;
+        private System.Windows.Forms.TextBox firstYBox;
+        private System.Windows.Forms.TextBox secondXBox;
+        private System.Windows.Forms.TextBox secondYBox;
+        private System.Windows.Forms.ListView linesView;
+    }
+}
---
+++ WindowsFormsControlLibrary1/UserControl1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsControlLibrary1
+{
+    public partial class UserControl1: UserControl
+    {
+        private List<Line> lines = new List<Line>();
+        public UserControl1()
+        {
+            InitializeComponent();
+            Paint += splitContainer1_Panel1_Paint;
+            //lines.Add(new Line(new Point(30, 20), new Point(10, 40)));
+        }
+
+        private void sevenButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+            //Graphics graphics = this.splitContainer1.Panel2.CreateGraphics();
+            Pen blackPen = new Pen(Color.Black, 3);
+            foreach (var line in lines)
+            {
+                System.Diagnostics.Debug.WriteLine("draw");
+                System.Diagnostics.Debug.WriteLine(line.Point1);
+                System.Diagnostics.Debug.WriteLine(line.Point2);
+
+                e.Graphics.DrawLine(blackPen, line.Point1, line.Point2);
+            }
+
+            //Point p1 = new Point(10, 10);
+            //Point p2 = new Point(50, 50);
+            //e.Graphics.DrawLine(blackPen, p1, p2);
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void findIntersectionsButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Added");
+            int firstX = int.Parse(firstXBox.Text);
+            int firstY = int.Parse(firstYBox.Text);
+            int secondX = int.Parse(secondXBox.Text);
+            int secondY = int.Parse(secondYBox.Text);
+            Point point1 = new Point(firstX, firstY);
+            Point point2 = new Point(secondX, secondY);
+            lines.Add(new Line(point1, point2));
+            //this.Invalidate();
+            this.Refresh();
+        }
+    }
+
+    public class Line
+    {
+        private Point point1;
+        private Point point2;
+        private Color color;
+
+        public Line(Point point1, Point point2)
+        {
+            this.point1 = point1;
+            this.point2 = point2;
+            color = Color.Black;
+        }
+
+        public Line(Point point1, Point point2, Color color)
+        {
+            this.point1 = point1;
+            this.point2 = point2;
+            this.color = color;
+        }
+
+        public Color Color { get => color; set => color = value; }
+        public Point Point2 { get => point2; set => point2 = value; }
+        public Point Point1 { get => point1; set => point1 = value; }
+    }
+}
