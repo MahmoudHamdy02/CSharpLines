c13dad2e8ccc139ead98e999c010a7eeec6fb1dd
Mohamed Adham <100296264+mohamedAdhamc@users.noreply.github.com>
2023-05-13 05:29:14 +0200

    update (too lazy)

--- WindowsFormsControlLibrary1/UserControl1.Designer.cs
+++ WindowsFormsControlLibrary1/UserControl1.Designer.cs
@@ -55,6 +55,7 @@
             this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
@@ -65,8 +66,9 @@
             // 
             this.splitContainer1.Panel2.Controls.Add(this.tableLayoutPanel1);
             this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
-            this.splitContainer1.Size = new System.Drawing.Size(1159, 577);
-            this.splitContainer1.SplitterDistance = 732;
+            this.splitContainer1.Size = new System.Drawing.Size(993, 469);
+            this.splitContainer1.SplitterDistance = 627;
+            this.splitContainer1.SplitterWidth = 3;
             this.splitContainer1.TabIndex = 0;
             // 
             // tableLayoutPanel1
@@ -93,32 +95,36 @@
             this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 4;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 76F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(419, 573);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(359, 465);
             this.tableLayoutPanel1.TabIndex = 0;
             this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
             // 
             // colorButton
             // 
             this.colorButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.colorButton.Location = new System.Drawing.Point(3, 93);
+            this.colorButton.Location = new System.Drawing.Point(3, 76);
+            this.colorButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.colorButton.Name = "colorButton";
-            this.colorButton.Size = new System.Drawing.Size(77, 39);
+            this.colorButton.Size = new System.Drawing.Size(65, 33);
             this.colorButton.TabIndex = 1;
             this.colorButton.Text = "Color";
             this.colorButton.UseVisualStyleBackColor = true;
+            this.colorButton.Click += new System.EventHandler(this.colorButton_Click);
             // 
             // updateButton
             // 
             this.updateButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.updateButton.Location = new System.Drawing.Point(86, 93);
+            this.updateButton.Location = new System.Drawing.Point(74, 76);
+            this.updateButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.updateButton.Name = "updateButton";
-            this.updateButton.Size = new System.Drawing.Size(77, 39);
+            this.updateButton.Size = new System.Drawing.Size(65, 33);
             this.updateButton.TabIndex = 1;
             this.updateButton.Text = "Update";
             this.updateButton.UseVisualStyleBackColor = true;
@@ -126,20 +132,23 @@
             // removeButton
             // 
             this.removeButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.removeButton.Location = new System.Drawing.Point(169, 93);
+            this.removeButton.Location = new System.Drawing.Point(145, 76);
+            this.removeButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.removeButton.Name = "removeButton";
-            this.removeButton.Size = new System.Drawing.Size(77, 39);
+            this.removeButton.Size = new System.Drawing.Size(65, 33);
             this.removeButton.TabIndex = 2;
             this.removeButton.Text = "Remove";
             this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
             // 
             // findIntersectionsButton
             // 
             this.tableLayoutPanel1.SetColumnSpan(this.findIntersectionsButton, 2);
             this.findIntersectionsButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.findIntersectionsButton.Location = new System.Drawing.Point(252, 93);
+            this.findIntersectionsButton.Location = new System.Drawing.Point(216, 76);
+            this.findIntersectionsButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.findIntersectionsButton.Name = "findIntersectionsButton";
-            this.findIntersectionsButton.Size = new System.Drawing.Size(164, 39);
+            this.findIntersectionsButton.Size = new System.Drawing.Size(140, 33);
             this.findIntersectionsButton.TabIndex = 3;
             this.findIntersectionsButton.Text = "Find Intersections";
             this.findIntersectionsButton.UseVisualStyleBackColor = true;
@@ -148,10 +157,11 @@
             // addButton
             // 
             this.addButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.addButton.Location = new System.Drawing.Point(335, 3);
+            this.addButton.Location = new System.Drawing.Point(287, 2);
+            this.addButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.addButton.Name = "addButton";
             this.tableLayoutPanel1.SetRowSpan(this.addButton, 2);
-            this.addButton.Size = new System.Drawing.Size(81, 84);
+            this.addButton.Size = new System.Drawing.Size(69, 70);
             this.addButton.TabIndex = 5;
             this.addButton.Text = "Add";
             this.addButton.UseVisualStyleBackColor = true;
@@ -162,55 +172,59 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(3, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.Size = new System.Drawing.Size(36, 13);
             this.label1.TabIndex = 6;
             this.label1.Text = "First X";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(169, 0);
+            this.label3.Location = new System.Drawing.Point(145, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(66, 17);
+            this.label3.Size = new System.Drawing.Size(54, 13);
             this.label3.TabIndex = 8;
             this.label3.Text = "Second X";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(169, 45);
+            this.label4.Location = new System.Drawing.Point(145, 37);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(66, 17);
+            this.label4.Size = new System.Drawing.Size(54, 13);
             this.label4.TabIndex = 9;
             this.label4.Text = "Second Y";
             // 
             // firstXBox
             // 
-            this.firstXBox.Location = new System.Drawing.Point(86, 3);
+            this.firstXBox.Location = new System.Drawing.Point(74, 2);
+            this.firstXBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.firstXBox.Name = "firstXBox";
-            this.firstXBox.Size = new System.Drawing.Size(77, 24);
+            this.firstXBox.Size = new System.Drawing.Size(65, 20);
             this.firstXBox.TabIndex = 10;
             this.firstXBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
             // 
             // firstYBox
             // 
-            this.firstYBox.Location = new System.Drawing.Point(86, 48);
+            this.firstYBox.Location = new System.Drawing.Point(74, 39);
+            this.firstYBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.firstYBox.Name = "firstYBox";
-            this.firstYBox.Size = new System.Drawing.Size(77, 24);
+            this.firstYBox.Size = new System.Drawing.Size(65, 20);
             this.firstYBox.TabIndex = 10;
             // 
             // secondXBox
             // 
-            this.secondXBox.Location = new System.Drawing.Point(252, 3);
+            this.secondXBox.Location = new System.Drawing.Point(216, 2);
+            this.secondXBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.secondXBox.Name = "secondXBox";
-            this.secondXBox.Size = new System.Drawing.Size(77, 24);
+            this.secondXBox.Size = new System.Drawing.Size(65, 20);
             this.secondXBox.TabIndex = 10;
             // 
             // secondYBox
             // 
-            this.secondYBox.Location = new System.Drawing.Point(252, 48);
+            this.secondYBox.Location = new System.Drawing.Point(216, 39);
+            this.secondYBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.secondYBox.Name = "secondYBox";
-            this.secondYBox.Size = new System.Drawing.Size(77, 24);
+            this.secondYBox.Size = new System.Drawing.Size(65, 20);
             this.secondYBox.TabIndex = 10;
             // 
             // linesView
@@ -218,28 +232,33 @@
             this.tableLayoutPanel1.SetColumnSpan(this.linesView, 5);
             this.linesView.Dock = System.Windows.Forms.DockStyle.Fill;
             this.linesView.HideSelection = false;
-            this.linesView.Location = new System.Drawing.Point(3, 138);
+            this.linesView.Location = new System.Drawing.Point(3, 113);
+            this.linesView.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.linesView.MultiSelect = false;
             this.linesView.Name = "linesView";
-            this.linesView.Size = new System.Drawing.Size(413, 432);
+            this.linesView.Size = new System.Drawing.Size(353, 350);
             this.linesView.TabIndex = 11;
             this.linesView.UseCompatibleStateImageBehavior = false;
+            this.linesView.View = System.Windows.Forms.View.List;
+            this.linesView.SelectedIndexChanged += new System.EventHandler(this.linesView_SelectedIndexChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 45);
+            this.label2.Location = new System.Drawing.Point(3, 37);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.Size = new System.Drawing.Size(36, 13);
             this.label2.TabIndex = 7;
             this.label2.Text = "First Y";
             // 
             // UserControl1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.splitContainer1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "UserControl1";
-            this.Size = new System.Drawing.Size(1159, 577);
+            this.Size = new System.Drawing.Size(993, 469);
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
--- WindowsFormsControlLibrary1/UserControl1.cs
+++ WindowsFormsControlLibrary1/UserControl1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace WindowsFormsControlLibrary1
 {
@@ -57,19 +58,21 @@ namespace WindowsFormsControlLibrary1
         private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
         {
             //Graphics graphics = this.splitContainer1.Panel2.CreateGraphics();
-            Pen blackPen = new Pen(Color.Black, 3);
+            Pen pen = new Pen(Color.Black, 3);
             foreach (var line in lines)
             {
                 System.Diagnostics.Debug.WriteLine("draw");
                 System.Diagnostics.Debug.WriteLine(line.Point1);
                 System.Diagnostics.Debug.WriteLine(line.Point2);
-
-                e.Graphics.DrawLine(blackPen, line.Point1, line.Point2);
+                //set the color to draw the line with
+                if (line.Color != pen.Color)
+                    pen.Color = line.Color;
+                e.Graphics.DrawLine(pen, line.Point1, line.Point2);
             }
 
             //Point p1 = new Point(10, 10);
             //Point p2 = new Point(50, 50);
-            //e.Graphics.DrawLine(blackPen, p1, p2);
+            //e.Graphics.DrawLine(pen, p1, p2);
         }
 
         private void textBox1_TextChanged_1(object sender, EventArgs e)
@@ -84,6 +87,7 @@ namespace WindowsFormsControlLibrary1
 
         private void addButton_Click(object sender, EventArgs e)
         {
+            //To Do: add error handling   
             System.Diagnostics.Debug.WriteLine("Added");
             int firstX = int.Parse(firstXBox.Text);
             int firstY = int.Parse(firstYBox.Text);
@@ -91,9 +95,71 @@ namespace WindowsFormsControlLibrary1
             int secondY = int.Parse(secondYBox.Text);
             Point point1 = new Point(firstX, firstY);
             Point point2 = new Point(secondX, secondY);
-            lines.Add(new Line(point1, point2));
+            lines.Add(new Line(point1, point2, colorButton.ForeColor));
+
+            //add to list
+            ListViewItem item = new ListViewItem();
+            item.Text = "First point (x=" + firstX + ",y=" + firstY + ") Second point (x=" + secondX + ",y=" + secondY+")";
+            linesView.Items.Add(item);
+
             //this.Invalidate();
             this.Refresh();
+        }
+
+        private void colorButton_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("on color button clicked");
+            // Create a new instance of the ColorDialog
+            ColorDialog colorDialog = new ColorDialog();
+
+            // Show the color dialog and capture the result
+            DialogResult result = colorDialog.ShowDialog();
+
+            // Check if the user clicked the OK button in the dialog
+            if (result == DialogResult.OK)
+            {
+                // Retrieve the selected color
+                Color selectedColor = colorDialog.Color;
+                colorButton.ForeColor = selectedColor;
+
+                // Do something with the selected color
+                Console.WriteLine("Selected color: " + selectedColor);
+            }
+
+        }
+
+        private void linesView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //the implementation is based on keeping the list in the same order as our internal line list
+        //To Do: add error messages
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            // Get the index of the selected item
+            int selectedIndex = linesView.SelectedIndices.Count > 0 ? linesView.SelectedIndices[0] : -1;
+
+            // Check if an item is selected and if so remove it from both our lists
+            if (selectedIndex != -1)
+            {
+                System.Diagnostics.Debug.WriteLine("Selected index: " + selectedIndex);
+                linesView.Items.RemoveAt(selectedIndex);
+                // Check if the target index exists if not check the one before it
+                if (selectedIndex >= 0 && selectedIndex < linesView.Items.Count)
+                {
+                    // Select the item at the target index
+                    linesView.Items[selectedIndex].Selected = true;
+                    linesView.Select(); // set focus to it
+                } else if (selectedIndex - 1 >=0 && selectedIndex < linesView.Items.Count)
+                {
+                    linesView.Items[selectedIndex - 1].Selected = true;
+                    linesView.Select(); // set focus to it
+                }
+                lines.RemoveAt(selectedIndex);
+                //update the drawing
+                this.Refresh();
+            }
         }
     }
 
@@ -120,5 +186,7 @@ namespace WindowsFormsControlLibrary1
         public Color Color { get => color; set => color = value; }
         public Point Point2 { get => point2; set => point2 = value; }
         public Point Point1 { get => point1; set => point1 = value; }
+
+        //override tostring
     }
 }
